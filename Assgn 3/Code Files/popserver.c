59bfe0bbd60068edcd25adc2c2bd02e6be64f9d5
Illumine69 <sanskarmittal2002@gmail.com>
2024-02-08 19:44:19 +0530

    testing popserver.c

--- Assgn 3/Code Files/popserver.c
+++ Assgn 3/Code Files/popserver.c
@@ -18,13 +18,13 @@ File: popserver.c
 #include <time.h>
 #define MAX 5000
 
-void recvData(int sockfd, char* buf, char* mainBuf, int size, int flags, char* expected, char* errMsg, char* errExpected){
+void recvData(int sockfd, char* buf, char* mainBuf, int size, int flags, char* expected, char* errMsg, char* errExpected,int checkExpected){
     int n;
     
     memset(mainBuf, '\0', MAX);
     memset(buf, '\0', MAX);
 
-    while(n = recv(sockfd, buf, size, flags)){
+    while((n = recv(sockfd, buf, size, flags))){
         if(n == -1){
             perror(errMsg);
             exit(EXIT_FAILURE);
@@ -34,7 +34,7 @@ void recvData(int sockfd, char* buf, char* mainBuf, int size, int flags, char* e
         strncat(mainBuf, buf, n);
 
         if(strlen(mainBuf) >= strlen(expected)){
-            if(strncmp(mainBuf, expected, strlen(expected)) != 0){
+            if(checkExpected && (strncmp(mainBuf, expected, strlen(expected)) != 0)){
                 perror(errExpected);
                 exit(EXIT_FAILURE);
             }
@@ -117,12 +117,8 @@ int main(int argc, char* argv[]){
             char* user = (char*)malloc((MAX)*sizeof(char));
             char* password = (char*)malloc((MAX)*sizeof(char));
             char* userLine = (char*)malloc(MAX*sizeof(char));
-            char* mailFrom = (char*)malloc((MAX)*sizeof(char));     // This also includes sender's domain name
-            char* mailTo = (char*)malloc((MAX)*sizeof(char));
             char* mail = (char*)malloc((MAX + 10)*sizeof(char));
-            char* recvTime = (char*)malloc((100)*sizeof(char));
             char* userFileName = (char*)malloc((MAX + 10)*sizeof(char));
-            char* domain = (char*)malloc((MAX + 10)*sizeof(char));
 
             memset(mainBuf, '\0', MAX);
             memset(buf, '\0', MAX);
@@ -130,11 +126,7 @@ int main(int argc, char* argv[]){
             memset(password, '\0', MAX);
             memset(userLine, '\0', MAX);
             memset(mail, '\0', MAX);
-            memset(mailFrom, '\0', MAX);
-            memset(mailTo, '\0', MAX);
-            memset(recvTime, '\0', 100);
             memset(userFileName, '\0', MAX);
-            memset(domain, '\0', MAX);
 
             // Send SERVER READY
             sprintf(buf, "+OK POP3 server ready\r\n");
@@ -143,7 +135,7 @@ int main(int argc, char* argv[]){
             /* AUTHORIZATION STATE */
 
             // Receive USER
-            recvData(newsockfd, buf, mainBuf, MAX, 0, "USER ", "Error in receiving USER\n", "USER not received\n");
+            recvData(newsockfd, buf, mainBuf, MAX, 0, "USER ", "Error in receiving USER\n", "USER not received\n",1);
             sscanf(mainBuf, "USER %s", user);
 
             // check if mailbox of such user exists
@@ -162,7 +154,7 @@ int main(int argc, char* argv[]){
             sendData(newsockfd, buf, 0, "Client closed connection at USER OK\n", "Error in sending USER OK\n");
 
             // Receive PASS
-            recvData(newsockfd, buf, mainBuf, MAX, 0, "PASS ", "Error in receiving PASS\n", "PASS not received\n");
+            recvData(newsockfd, buf, mainBuf, MAX, 0, "PASS ", "Error in receiving PASS\n", "PASS not received\n",1);
             sscanf(mainBuf, "PASS %s", password);
 
             // check if user and password is correct in user.txt
@@ -191,24 +183,217 @@ int main(int argc, char* argv[]){
 
             // Find message number and size in user's mailbox(to be used in STAT)
             sprintf(userFileName, "%s/mymailbox", user);
-            FILE* userMailbox = fopen(userFileName, "r");
+            FILE* userMailbox;
+
+            if((userMailbox = fopen(userFileName, "r")) == NULL){
+                sprintf(buf, "-ERR unable to open mailbox for %s\r\n", user);
+                sendData(newsockfd, buf, 0, "Client closed connection at MAILBOX not found\n", "Error in sending MAILBOX not found\n");
+                close(newsockfd);
+                exit(EXIT_FAILURE);
+            }
 
             int mailNum = 0;
-            int mailSize = 0;
+            int totalMailSize = 0;
            
-            // increase mailNum on every <.\n> line
             while(fgets(mail, MAX, userMailbox) != NULL){
-                mailSize += strlen(mail);
+                totalMailSize += (strlen(mail) + 1);
                 if(mail[0] == '.' && mail[1] == '\n'){
                     mailNum++;
                 }
             }
-            fclose(userMailbox);
+
+            int mailSize[mailNum], curMailNum = 0;
+            int curMailSize = 0;
+            memset(mailSize, 0, mailNum*sizeof(int));
+            while(fgets(mail, MAX, userMailbox) != NULL){
+                curMailSize += (strlen(mail) + 1);
+                if(mail[0] == '.' && mail[1] == '\n'){
+                    mailSize[curMailNum] = curMailSize;
+                    curMailSize = 0;
+                    curMailNum++;
+                }
+            }
 
             // Send PASS OK
-            sprintf(buf, "+OK %s's maildrop has %d messages (%d octets)\r\n", user, mailNum, mailSize);
+            sprintf(buf, "+OK %s's maildrop has %d messages (%d octets)\r\n", user, mailNum, totalMailSize);
+            sendData(newsockfd, buf, 0, "Client closed connection at PASS OK\n", "Error in sending PASS OK\n");
 
             /* TRANSACTION STATE */
+            curMailNum = mailNum;
+
+            while(1){
+                // Receive command
+                recvData(newsockfd, buf, mainBuf, MAX, 0, "STAT", "Error in receiving command\n", "Command not received\n",0);
+
+                // if command is QUIT
+                if(strncmp(mainBuf, "QUIT", 4) == 0){
+                    sprintf(buf, "+OK %s POP3 server signing off\r\n", user);
+                    sendData(newsockfd, buf, 0, "Client closed connection at QUIT\n", "Error in sending QUIT\n");
+                    break;
+                }
+
+                // if command is STAT
+                if(strncmp(mainBuf, "STAT", 4) == 0){
+                    sprintf(buf, "+OK %d %d\r\n", curMailNum, totalMailSize);
+                    sendData(newsockfd, buf, 0, "Client closed connection at STAT\n", "Error in sending STAT\n");
+                }
+
+                // if command is LIST
+                if(strncmp(mainBuf, "LIST", 4) == 0){
+                    int num = 0;
+                    // check if LIST is followed by a number
+                    if(strlen(mainBuf) > 6){
+                        sscanf(mainBuf, "LIST %d", &num);
+                        if(num > mailNum || num < 1){
+                            sprintf(buf, "-ERR no such message, only %d messages in maildrop\r\n", mailNum);
+                            sendData(newsockfd, buf, 0, "Client closed connection at LIST OUTOFBOUND\n", "Error in sending LIST OUTOFBOUND\n");
+                        }
+                        else if(mailSize[num - 1] == 0){
+                            sprintf(buf, "-ERR message %d has been deleted\r\n", num);
+                            sendData(newsockfd, buf, 0, "Client closed connection at LIST DELETED\n", "Error in sending LIST DELETED\n");
+                        }
+                        else{
+                            sprintf(buf, "+OK %d %d\r\n", num, mailSize[num - 1]);
+                            sendData(newsockfd, buf, 0, "Client closed connection at LIST\n", "Error in sending LIST\n");
+                        }
+                    }
+                    else{
+                        sprintf(buf, "+OK %d messages (%d octets)\r\n", curMailNum, totalMailSize);
+                        sendData(newsockfd, buf, 0, "Client closed connection at LIST ALL\n", "Error in sending LIST ALL\n");
+                        for(int i = 0; i < mailNum; i++){
+                            if(mailSize[i] == 0) continue;
+                            sprintf(buf, "%d %d\r\n", i + 1, mailSize[i]);
+                            sendData(newsockfd, buf, 0, "Client closed connection at LIST\n", "Error in sending LIST\n");
+                        }
+                        sprintf(buf, ".\r\n");
+                        sendData(newsockfd, buf, 0, "Client closed connection at LIST COMPLETE\n", "Error in sending LIST COMPLETE\n");
+                    }
+                }
+
+                // if command is RETR
+                if(strncmp(mainBuf, "RETR", 4) == 0){
+                    int num = 0;
+                    sscanf(mainBuf, "RETR %d", &num);
+                    if(num > mailNum || num < 1){
+                        sprintf(buf, "-ERR no such message, only %d messages in maildrop\r\n", mailNum);
+                        sendData(newsockfd, buf, 0, "Client closed connection at RETR OUTOFBOUND\n", "Error in sending RETR OUTOFBOUND\n");
+                    }
+                    else if(mailSize[num - 1] == 0){
+                        sprintf(buf, "-ERR message %d has been deleted\r\n", num);
+                        sendData(newsockfd, buf, 0, "Client closed connection at RETR DELETED\n", "Error in sending RETR DELETED\n");
+                    }
+                    else{
+                        sprintf(buf, "+OK %d octets\r\n", mailSize[num - 1]);
+                        sendData(newsockfd, buf, 0, "Client closed connection at RETR\n", "Error in sending RETR\n");
+
+                        // Send the mail
+                        int mailFound = 0;
+                        while(fgets(mail, MAX, userMailbox) != NULL){
+                            if(mailFound == (num-1)){
+                                mail[strlen(mail) - 1] = '\r';
+                                mail[strlen(mail)] = '\n';
+                                mail[strlen(mail) + 1] = '\0';
+                                sendData(newsockfd, mail, 0, "Client closed connection at RETR MAIL\n", "Error in sending RETR MAIL\n");
+                            }
+                            if(mail[0] == '.'){
+                                mailFound++;
+                            }
+                            if(mailFound == num){
+                                break;
+                            }
+                        }
+                    }
+                }
+
+                // if command is DELE
+                if(strncmp(mainBuf, "DELE", 4) == 0){
+                    int num = 0;
+                    sscanf(mainBuf, "DELE %d", &num);
+                    if(num > mailNum || num < 1){
+                        sprintf(buf, "-ERR no such message, only %d messages in maildrop\r\n", mailNum);
+                        sendData(newsockfd, buf, 0, "Client closed connection at DELE OUTOFBOUND\n", "Error in sending DELE OUTOFBOUND\n");
+                    }
+                    else if(mailSize[num - 1] == 0){
+                        sprintf(buf, "-ERR message %d already deleted\r\n", num);
+                        sendData(newsockfd, buf, 0, "Client closed connection at DELE DELETED\n", "Error in sending DELE DELETED\n");
+                    }
+                    else{
+                        totalMailSize -= mailSize[num - 1];
+                        mailSize[num - 1] = 0;
+                        curMailNum--;
+                        sprintf(buf, "+OK message %d deleted\r\n", num);
+                        sendData(newsockfd, buf, 0, "Client closed connection at DELE\n", "Error in sending DELE\n");
+                    }
+                }
+
+                // if command is NOOP
+                if(strncmp(mainBuf, "NOOP", 4) == 0){
+                    sprintf(buf, "+OK\r\n");
+                    sendData(newsockfd, buf, 0, "Client closed connection at NOOP\n", "Error in sending NOOP\n");
+                }
+
+                // if command is RSET
+                if(strncmp(mainBuf, "RSET", 4) == 0){
+                    memset(mail, '\0', MAX);
+                    curMailSize = 0;
+                    totalMailSize = 0;
+                    curMailNum = 0;
+                    while(fgets(mail, MAX, userMailbox) != NULL){
+                        curMailSize += (strlen(mail) + 1);
+                        totalMailSize += curMailSize;
+                        if(mail[0] == '.' && mail[1] == '\n'){
+                            mailSize[curMailNum] = curMailSize;
+                            curMailSize = 0;
+                            curMailNum++;
+                        }
+                    }
+                    
+                    sprintf(buf, "+OK maildrop has %d messages (%d octets)\r\n", mailNum, totalMailSize);
+                    sendData(newsockfd, buf, 0, "Client closed connection at RSET\n", "Error in sending RSET\n");
+                }
+
+            }
+
+            /* UPDATE STATE */
+            mailNum = curMailNum;
+            curMailNum = 0;
+
+            // create a temporary file
+            FILE* tempFile = fopen("tempfile", "w+");
+
+            while(fgets(mail, MAX, userMailbox) != NULL){
+                if(mailSize[curMailNum] != 0){
+                    fprintf(tempFile, "%s", mail);
+                }
+                if(mail[0] == '.' && mail[1] == '\n'){
+                    curMailNum++;
+                }
+            }
+
+            // clean the user mailbox
+            userMailbox = fopen(userFileName, "w+");
+
+            while(fgets(mail, MAX, tempFile) != NULL){
+                fprintf(userMailbox, "%s", mail);
+            }
+
+            // delete the temporary file
+            if(remove("tempfile") == -1){
+                perror("Error in deleting temporary file\n");
+                exit(EXIT_FAILURE);
+            }
+            sprintf(buf, "+OK POP3 server signing off (%s messages left)\r\n", mailNum);
+            sendData(newsockfd, buf, 0, "Client closed connection at UPDATE\n", "Error in sending UPDATE\n");
+
+            fclose(tempFile);
+            fclose(userMailbox);
+            free(buf);
+            free(mainBuf);
+            free(user);
+            free(password);
+            free(userLine);
+            free(mail);
+            free(userFileName);
 
             close(newsockfd);
             exit(EXIT_SUCCESS);
