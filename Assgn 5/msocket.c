78189de5c5ad9d2da0768695c965cafcb1bebbf1
vkr2471 <vkr2471@gmail.com>
2024-03-05 17:45:06 +0530

    m_bind finished

--- Assgn 5/msocket.c
+++ Assgn 5/msocket.c
@@ -40,20 +40,44 @@ int m_socket(int domain, int type, int protocol){
 }
 
 int m_bind(int m_sockfd, const struct sockaddr *src_addr, socklen_t src_addrlen, const struct sockaddr *dest_addr, socklen_t dest_addrlen){
+
+
+    //initialze the buff?
     key_t key = KEY;
     int shmid = shmget(key, N*sizeof(struct shared_memory), 0777);
     struct shared_memory *SM = (struct shared_memory *)shmat(shmid, NULL, 0);
-    //find a free entry in shared memory
+   
 
-    //lock the shared memory
+    //lock the shared memory ? not required as only this process is aceesing when m_bind is in progress
 
 
     //fill the entry in shared memory
     SM[m_sockfd].addr = (struct sockaddr_in *)dest_addr;
+    // initialze   the send window
+    SM[m_sockfd].swnd.send_window_size=1;
+    SM[m_sockfd].swnd.last_ack=0;// COZ numbering starrts from 1
+    memset(SM[m_sockfd].swnd.unack_msg,-1,sizeof(SM[m_sockfd].swnd.unack_msg));
+    memset(SM[m_sockfd].swnd.unack_time,-1,sizeof(SM[m_sockfd].swnd.unack_time));
+    // initialze   the receive window
+    SM[m_sockfd].rwnd.receive_window_size=MAX_WINDOW_SIZE;
+    SM[m_sockfd].rwnd.last_inorder_msg=0;// COZ numbering starrts from 1
+    memset(SM[m_sockfd].rwnd.recv_msg,-1,sizeof(SM[m_sockfd].rwnd.recv_msg));
+    // call the system bind call
+    
+    int res =bind(SM[m_sockfd].sockfd, src_addr, src_addrlen);
 
 
+
+    //unlock the shared memory
+   
 
     //deatch from shared memory
+    shmdt(SM);
+
+
+
+     return res;
+
     
 
 
@@ -65,6 +89,10 @@ ssize_t m_sendto(int sockfd, const void *message, size_t length, int flags, cons
 
 ssize_t m_recvfrom(int sockfd, void *restrict buffer, size_t length, int flags, struct sockaddr *restrict address, socklen_t *restrict address_len){}
 
+
+
+
+//wrong implementation should give 
 int m_close(int fd){
     int res;
     if((res = close(fd)) == 0){     // Successfully closed the socket
